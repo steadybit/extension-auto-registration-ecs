50eb5accd2bd411d3557c7357a67acb39610d1f1
Daniel Reuter <daniel@steadybit.com>
2024-12-03 16:15:28 +0100

    chore: config parameters

--- config/specification.go
+++ config/specification.go
@@ -5,6 +5,7 @@ package config
 
 type Specification struct {
 	EcsClusterName    string   `json:"ecsClusterName" split_words:"true" required:"true"`
-	DiscoveryInterval int      `json:"discoveryInterval" split_words:"true" required:"false" default:"2"`
+	AgentKey          string   `json:"agentKey" split_words:"true" required:"true"`
+	DiscoveryInterval int      `json:"discoveryInterval" split_words:"true" required:"false" default:"30"`
 	TaskFamilies      []string `json:"taskFamilies" split_words:"true" required:"false" default:"steadybit-extension-host,steadybit-extension-container,steadybit-extension-http,steadybit-extension-aws"`
 }
