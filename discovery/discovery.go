194b2fbb2b07279e67ac2ab1dda3dffe28032924
Daniel Reuter <daniel@steadybit.com>
2024-12-04 07:00:01 +0100

    chore: implemented the extension registration

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -2,20 +2,30 @@ package discovery
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
 	"github.com/go-resty/resty/v2"
 	"github.com/rs/zerolog/log"
 	extensionconfig "github.com/steadybit/extension-discovery-ecs/config"
 	"github.com/steadybit/extension-kit/extutil"
+	"strings"
 )
 
 var (
-	taskDefinitionsCache map[string][]string
-	hostIpCache          map[string]string
+	hostIpCache map[string]string
 )
 
 func UpdateAgentExtensions(httpClient *resty.Client, awsClient *ecs.Client) {
+	currentRegistrations, err := getCurrentRegistrations(httpClient)
+	if err == nil {
+		discoveredExtensions := discoverExtensions(awsClient)
+		syncRegistrations(httpClient, &currentRegistrations, &discoveredExtensions)
+	}
+}
+
+func getCurrentRegistrations(httpClient *resty.Client) ([]extensionConfigAO, error) {
 	var currentRegistrations *[]extensionConfigAO
 	resp, err := httpClient.R().
 		SetHeader("Accept", "application/json").
@@ -24,16 +34,20 @@ func UpdateAgentExtensions(httpClient *resty.Client, awsClient *ecs.Client) {
 
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to get extension registrations from the agent. Skip Discovery.")
-		return
+		return nil, err
 	}
 	if resp.IsError() {
 		log.Error().Msgf("Failed to get extension registrations from the agent: %s. Skip Discovery.", resp.Status())
-		return
+		return nil, errors.New(fmt.Sprintf("Failed to get extension registrations from the agent: %s", resp.Status()))
 	}
 	if resp.IsSuccess() {
 		log.Debug().Int("count", len(*currentRegistrations)).Msg("Got extension registrations from the agent")
 	}
+	return *currentRegistrations, nil
+}
 
+func discoverExtensions(awsClient *ecs.Client) []extensionConfigAO {
+	discoveredExtensions := make([]extensionConfigAO, 0)
 	for _, taskFamily := range extensionconfig.Config.TaskFamilies {
 		listTasksOutput, err := awsClient.ListTasks(context.TODO(), &ecs.ListTasksInput{
 			Cluster:       &extensionconfig.Config.EcsClusterName,
@@ -41,8 +55,8 @@ func UpdateAgentExtensions(httpClient *resty.Client, awsClient *ecs.Client) {
 			Family:        &taskFamily,
 		})
 		if err != nil {
-			log.Error().Err(err).Msg("Failed to get tasks from ECS. Skip Discovery.")
-			return
+			log.Warn().Err(err).Msg("Failed to list tasks. No extensions discovered.")
+			return discoveredExtensions
 		}
 		if len(listTasksOutput.TaskArns) > 0 {
 			describeTasksOutput, err := awsClient.DescribeTasks(context.TODO(), &ecs.DescribeTasksInput{
@@ -51,8 +65,8 @@ func UpdateAgentExtensions(httpClient *resty.Client, awsClient *ecs.Client) {
 				Include: []types.TaskField{types.TaskFieldTags},
 			})
 			if err != nil {
-				log.Error().Err(err).Msg("Failed to describe tasks from ECS. Skip Discovery.")
-				return
+				log.Warn().Err(err).Msg("Failed to describe tasks. No extensions discovered.")
+				return discoveredExtensions
 			}
 			for _, task := range describeTasksOutput.Tasks {
 				portTag := getTagValue(task.Tags, "steadybit_extension_port")
@@ -74,10 +88,77 @@ func UpdateAgentExtensions(httpClient *resty.Client, awsClient *ecs.Client) {
 					ip = task.Containers[0].NetworkInterfaces[0].PrivateIpv4Address
 				}
 				if ip != nil {
-					log.Info().Msgf("Discovered Task: %s - %s:%s - %v", *task.Group, *ip, *portTag, *typesTag)
+					typesArray := strings.Split(*typesTag, ":")
+					discoveredExtensions = append(discoveredExtensions, extensionConfigAO{
+						Url:   "http://" + *ip + ":" + *portTag,
+						Types: typesArray,
+					})
+					log.Debug().Msgf("Discovered Task: %s - %s:%s - %v", *task.Group, *ip, *portTag, typesArray)
 				} else {
 					log.Warn().Msgf("Task: %s %s - No IP/Port found. Ignore.", *task.Group, *task.TaskArn)
 				}
+			}
+		}
+	}
+	return discoveredExtensions
+}
+
+func syncRegistrations(httpClient *resty.Client, currentRegistrations *[]extensionConfigAO, discoveredExtensions *[]extensionConfigAO) {
+	removeMissingRegistrations(httpClient, currentRegistrations, discoveredExtensions)
+	addNewRegistrations(httpClient, currentRegistrations, discoveredExtensions)
+}
+
+func removeMissingRegistrations(httpClient *resty.Client, currentRegistrations *[]extensionConfigAO, discoveredExtensions *[]extensionConfigAO) {
+	for _, currentRegistration := range *currentRegistrations {
+		found := false
+		for _, discoveredExtension := range *discoveredExtensions {
+			if currentRegistration.Url == discoveredExtension.Url {
+				found = true
+				break
+			}
+		}
+		if !found {
+			resp, err := httpClient.R().
+				SetHeader("Content-Type", "application/json").
+				SetBasicAuth("_", extensionconfig.Config.AgentKey).
+				SetBody(currentRegistration).
+				Delete("/extensions")
+			if err != nil {
+				log.Error().Err(err).Msgf("Failed to remove extension: %s", currentRegistration.Url)
+			}
+			if resp.IsError() {
+				log.Error().Msgf("Failed to remove extension: %s. Status: %s", currentRegistration.Url, resp.Status())
+			}
+			if resp.IsSuccess() {
+				log.Info().Msgf("Removed extension: %s", currentRegistration.Url)
+			}
+		}
+	}
+}
+
+func addNewRegistrations(httpClient *resty.Client, currentRegistrations *[]extensionConfigAO, discoveredExtensions *[]extensionConfigAO) {
+	for _, discoveredExtension := range *discoveredExtensions {
+		found := false
+		for _, currentRegistration := range *currentRegistrations {
+			if currentRegistration.Url == discoveredExtension.Url {
+				found = true
+				break
+			}
+		}
+		if !found {
+			resp, err := httpClient.R().
+				SetHeader("Content-Type", "application/json").
+				SetBasicAuth("_", extensionconfig.Config.AgentKey).
+				SetBody(discoveredExtension).
+				Post("/extensions")
+			if err != nil {
+				log.Error().Err(err).Msgf("Failed to add extension: %s", discoveredExtension.Url)
+			}
+			if resp.IsError() {
+				log.Error().Msgf("Failed to add extension: %s. Status: %s", discoveredExtension.Url, resp.Status())
+			}
+			if resp.IsSuccess() {
+				log.Info().Msgf("Added extension: %s", discoveredExtension.Url)
 			}
 		}
 	}
